2e4927a928a4a8ac3777dd18edea44fea0c3b6ab
Ay-slim <ayooluwaadedipe@gmail.com>
2023-12-26 21:24:08 +0000

    Test out how nullifying na object variable works

--- oop/overloading/Product.java
+++ oop/overloading/Product.java
@@ -32,5 +32,11 @@ public class Product {
     System.out.println(tapeMeasure);
     System.out.println(plaster);
     System.out.println(tyre);
+
+    //Testing how the system works when a variable previously pointing to an object is reassigned to null. Apparently it's just a pointer switch, the underlying object itself is not changed to null, it's the variable that now points to null
+    Product tyreClone = tyre;
+    tyre = null;
+    System.out.println("Clone: " + tyreClone);
+    System.out.println("Original: " + tyre);
   }
 }
