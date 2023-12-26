81281e93499cb000a4c65444f32b5db1adbce06f
Ay-slim <ayooluwaadedipe@gmail.com>
2023-12-26 21:55:05 +0000

    Payment card class

---
+++ oop/PaymentCard.java
@@ -0,0 +1,41 @@
+package oop;
+
+public class PaymentCard {
+  private double balance;
+
+  public PaymentCard(double balance) {
+      this.balance = balance;
+  }
+
+  public double balance() {
+      return this.balance;
+  }
+
+  public void addMoney(double increase) {
+      this.balance = this.balance + increase;
+  }
+
+  public boolean takeMoney(double amount) {
+    // implement the method so that it only takes money from the card if
+    // the balance is at least the amount parameter.
+    // returns true if successful and false otherwise
+    if (this.balance >= amount) {
+      this.balance = this.balance - amount;
+      return true;
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    PaymentCard petesCard = new PaymentCard(10);
+
+    System.out.println("money " + petesCard.balance());
+    boolean wasSuccessful = petesCard.takeMoney(8);
+    System.out.println("successfully withdrew: " + wasSuccessful);
+    System.out.println("money " + petesCard.balance());
+
+    wasSuccessful = petesCard.takeMoney(4);
+    System.out.println("successfully withdrew: " + wasSuccessful);
+    System.out.println("money " + petesCard.balance());
+}
+}
